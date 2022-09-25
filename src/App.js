e315ba1dcc98316d6dd9dee5e94a2c4ff409b5ae
MoncefME <ka_moussaoui@esi.dz>
2022-09-25 23:55:42 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import NavBar from './componants/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <div>
+         style me with tailwindcss
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='bg-slate-400 '>
+        NavBar
+    </div>
+  )
+}
+
+export default NavBar
